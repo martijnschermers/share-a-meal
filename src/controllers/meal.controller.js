4b5a2899760f8c3dae4488b705ccc184dfad0dcc
Martijn <martijnschermers2@gmail.com>
2022-05-13 12:39:49 +0200

    Added auth to meal endpoints

--- src/controllers/meal.controller.js
+++ src/controllers/meal.controller.js
@@ -1,17 +1,6 @@
 const Joi = require('joi');
 const database = require('../../database/database');
 
-// Placeholder for the time being, login functionality will be added later
-let loggedInUser = {
-  id: 1, 
-  firstName: "John",
-  lastName: "Beton",
-  street: "Lovensdijkstraat 61",
-  city: "Breda",
-  password: "secret",
-  emailAdress: "johndoe@gmail.com"
-}
-
 let controller = {
   validateMeal: (req, res, next) => {
     const schema = Joi.object({
@@ -32,7 +21,7 @@ let controller = {
       const error = {
         status: 400,
         // Error message wrapped variable in /" "\ for some reason
-        result: result.error.message.replace(/"/g, '')
+        message: result.error.message.replace(/"/g, '')
       };
       next(error);
     }
@@ -42,10 +31,7 @@ let controller = {
     database.getConnection(function (err, connection) {
       let meal = req.body;
       meal.allergenes = meal.allergenes.toString();
-
-      if (loggedInUser) {
-        meal.cookId = loggedInUser.id;
-      }
+      meal.cookId = req.userId;
 
       if (err) throw err;
 
@@ -94,7 +80,7 @@ let controller = {
         } else {
           const error = {
             status: 404,
-            result: 'Meal not found'
+            message: 'Meal not found'
           };
           next(error);
         }
@@ -124,7 +110,7 @@ let controller = {
           } else {
             const error = {
               status: 404,
-              result: 'Meal does not exist'
+              message: 'Meal does not exist'
             };
             next(error);
           }
@@ -148,7 +134,7 @@ let controller = {
         } else {
           const error = {
             status: 404,
-            result: 'Meal not found'
+            message: 'Meal not found'
           };
           next(error);
         }
@@ -161,48 +147,40 @@ let controller = {
 
       if (err) throw err;
 
-      connection.query(`SELECT * FROM meal WHERE id = ${id}; SELECT * FROM meal_participants_user WHERE mealId = ${id}`, function (error, results, fields) {
+      connection.query('SELECT * FROM meal WHERE id = ?; SELECT * FROM meal_participants_user WHERE mealId = ?;', [id, id], function (error, results, fields) {
         if (error) throw error;
 
         if (results[0].length > 0) {
-          if (loggedInUser) {
-            if (results[1][0].userId == loggedInUser.id) {
-              connection.query(`DELETE FROM meal_participants_user WHERE userId = ${loggedInUser.id} AND mealId = ${id};`, function (error, results, fields) {
+          if (results[1][0].userId == req.userId) {
+            connection.query(`DELETE FROM meal_participants_user WHERE userId = ? AND mealId = ?;`, [req.userId, id], function (error, results, fields) {
+              if (error) throw error;
+            });
+          } else {
+            if (results[1].length < results[0][0].maxAmountOfParticipants) {
+              connection.query('INSERT INTO meal_participants_user SET ?; SELECT * FROM meal_participants_user;', { mealId: id, userId: req.userId }, function (error, results, fields) {
+                connection.release();
                 if (error) throw error;
               });
             } else {
-              if (results[1].length < results[0][0].maxAmountOfParticipants) {
-                connection.query('INSERT INTO meal_participants_user SET ?; SELECT * FROM meal_participants_user;', { mealId: id, userId: loggedInUser.id }, function (error, results, fields) {
-                  connection.release();
-                  if (error) throw error;
-                });
-              } else {
-                const error = {
-                  status: 400,
-                  result: 'Meal is full'
-                };
-                next(error);
-              }
+              const error = {
+                status: 400,
+                message: 'Meal is full'
+              };
+              next(error);
             }
-
-            res.status(200).json({
-              status: 200,
-              result: {
-                currentlyParticipating: true,
-                currentAmountOfParticipants: results[1].length,
-              }
-            });
-          } else {
-            const error = {
-              status: 401,
-              result: 'You need to be logged in to participate'
-            }
-            next(error);
           }
+
+          res.status(200).json({
+            status: 200,
+            result: {
+              currentlyParticipating: true,
+              currentAmountOfParticipants: results[1].length,
+            }
+          });
         } else {
           const error = {
             status: 404,
-            result: 'Meal not found'
+            message: 'Meal not found'
           };
           next(error);
         }
--- src/routes/meal.routes.js
+++ src/routes/meal.routes.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/meal.controller');
+const authController = require('../controllers/auth.controller');
 
-router.post('/meal', controller.validateMeal, controller.addMeal);
+router.post('/meal', authController.validateToken, controller.validateMeal, controller.addMeal);
 
 router.get('/meal', controller.getAllMeals);
 
 router.get('/meal/:id', controller.getMealById);
 
-router.put('/meal/:id', controller.validateMeal, controller.updateMeal);
+router.put('/meal/:id', authController.validateToken, controller.validateMeal, controller.updateMeal);
 
-router.delete('/meal/:id', controller.deleteMeal);
+router.delete('/meal/:id', authController.validateToken, controller.deleteMeal);
 
-router.get('/meal/:id/participate', controller.participate);
+router.get('/meal/:id/participate', authController.validateToken, controller.participate);
 
 module.exports = router; 
