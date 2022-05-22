59e48fbfc49431c2a8a8a3310398517f5740b40d
Martijn <martijnschermers2@gmail.com>
2022-05-22 20:10:27 +0200

    Upped connectionLimit to 100

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  connectionLimit: 10,
+  connectionLimit: 100,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
